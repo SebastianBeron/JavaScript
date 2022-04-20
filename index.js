b63ab7b2638a2241929da089b4401bd964e0e76c
Sebastian Beron <nsebastianberon@gmail.com>
2022-04-20 20:16:39 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+/* Pedir número mediante prompt y sumarle otro número en cada repetición,
+realizando una salida por cada resultado */
+
+/* let ingresarNumero = parseInt(prompt("Ingrese Número"));
+
+for (let i = 0; i <= 10; i++){
+    let resultado = ingresarNumero + i;
+    alert(ingresarNumero + " + " + i + " = " + resultado);
+} */
+
+
+/* Pedir un texto mediante prompt, concatenar un valor en cada repetición, 
+realizando una salida por cada resultado, hasta que se ingresa “ESC”. */
+
+/* let entrada = prompt ("ingresar un dato");
+
+    while(entrada != "ESC" ) {
+    alert("el usuario ingresó "+ entrada);
+    entrada = prompt ("Ingresar otro dato");
+} */
+
+
+/* Pedir un número por prompt, repetir la salida del mensaje “Hola” 
+la cantidad de veces ingresada. */
+
+/* let ingresarNumero = parseInt(prompt("Ingrese Número"));
+
+    for(let i = 1; i <= ingresarNumero; i++){
+    alert("Hola");
+    } */
+
+
+
+
+
+
